988cef2f7f83ade6f7b6b3000b6aa80e21814340
yifattih <yifattih@proton.me>
2024-11-15 23:21:22 +0000

    chore(env): change FLASK_RUN_PORT env variable definition [skip ci]

--- app/src/wsgi.py
+++ app/src/wsgi.py
@@ -1,3 +1,4 @@
+import os
 from routes import app
 
 """
@@ -7,4 +8,5 @@ in the browser.
 """
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("FLASK_RUN_PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
