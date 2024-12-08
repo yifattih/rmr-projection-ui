ff659d5da9c46f115dec96732a7148f932da23e9
yifattih <yifattih@proton.me>
2024-12-08 09:57:55 +0000

    fix: add backend url and update to work with it

--- app/src/wsgi.py
+++ app/src/wsgi.py
@@ -5,10 +5,10 @@ from .routes import app
 Entry point to run the Flask application.
 
 Environment Variables:
-- PORT: The port number to run the application on (default: 5000).
+- PORT: The port number to run the application on (default: 4000).
 
 """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(debug=False, host="0.0.0.0", port=port)
