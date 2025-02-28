4bc4243fd59a8d19aa1fc17b2f0af6b4947ce73f
yifattih <yifattih@proton.me>
2025-02-28 19:26:21 +0000

    fix(service): add telemetry instrumentation in main.py for metrics, logs, and traces and update api url assigment from environment variable for readability

--- service/main.py
+++ service/main.py
@@ -10,29 +10,29 @@ service = Flask(__name__)
 
 setup_telemetry(service)
 
-try:
-    api_url = os.environ.get("API_URL")
-    assert api_url != None
-    api_health = {"message": requests.get(f"{api_url}/health").text,
-                  "status_code": requests.get(f"{api_url}/health").status_code}
-except AssertionError:
-    api_health = {"message": "API_URL environment variable not set",
-                  "status_code": 500}
-except requests.exceptions.MissingSchema as e:
-    api_health = {"message": "Wrong URL schema for API_URL",
-                  "status_code": 500}
+api_url = os.getenv("API_URL", "http://api:8000")
+logger.info(f"API URL: {api_url}")
 
-api_endpoint = "/rmr/"
-
+rmr_endpoint = f"{api_url}/rmr/"
+logger.info(f"RMR endpoint: {rmr_endpoint}")
 
 @service.get("/")
 def root() -> str:
     """
     Root endpoint that renders the index.html template.
     """
-    return render_template("index.html")
+    with tracer.start_as_current_span("root_span"):
+        logger.info("Root endpoint called")
 
-@service.route("/submit", methods=["POST"])
+        meter.create_counter(
+            name="root_endpoint_calls",
+            description="Counts the number of times the root endpoint is called",
+            unit="calls",
+        ).add(1)
+
+        return render_template("index.html")
+
+@service.post("/submit")
 def submit():
     """
     Endpoint to process submitted form data, call API for RMR calculations,
@@ -54,7 +54,7 @@ def submit():
             return jsonify({"error": f"Missing value for {key}"}), 400
 
     try:
-        response = requests.post(f"{api_url}{api_endpoint}", json=form_data)
+        response = requests.post(rmr_endpoint, json=form_data)
         response.raise_for_status()
         response_data = response.json()
     except requests.exceptions.RequestException as e:
@@ -69,12 +69,41 @@ def health_check():
     """
     Endpoint that returns the status of the service.
     """
-    current_time_utc = datetime.now(timezone.utc)
-    uptime_utc = current_time_utc - service_start_time_utc
+    with tracer.start_as_current_span("health_span") as span:
+        logger.info("Health check endpoint called")
 
-    health_status = {
-        "status": "healthy",
-        "uptime_utc": str(uptime_utc),
-        "timestamp": str(current_time_utc.isoformat()),
-    }
-    return health_status, 200
+        meter.create_counter(
+            name="health_check_requests",
+            description="Counts the number of health check requests",
+            unit="requests",
+        ).add(1)
+
+        current_time_utc = datetime.now(timezone.utc)
+        uptime_utc = current_time_utc - service_start_time_utc
+
+        span.set_attribute("health_check.uptime_utc", str(uptime_utc))
+
+        logger.info("Service uptime UTC: %s", uptime_utc)
+
+        health_status = {
+            "status": "healthy",
+            "uptime_utc": str(uptime_utc),
+            "timestamp": str(current_time_utc.isoformat()),
+        }
+
+        logger.info("Health check status: %s", health_status)
+
+        meter.create_counter(
+            name="health_check_successes",
+            description="Counts the number of successful health checks",
+            unit="successes",
+        ).add(1)
+
+        # Measure service uptime
+        meter.create_histogram(
+            name="service_uptime",
+            description="Measures the service uptime in seconds",
+            unit="seconds",
+        ).record(uptime_utc.total_seconds())
+
+        return health_status, 200
