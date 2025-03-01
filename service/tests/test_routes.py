07a94a48a3bc8f4f2df75de7853c056ee52847c7
yifattih <yifattih@proton.me>
2025-03-01 19:05:44 +0000

    refactor(service): update test_routes.py based on flake8 to maintain code quality standard

--- service/tests/test_routes.py
+++ service/tests/test_routes.py
@@ -1,13 +1,16 @@
+from unittest.mock import MagicMock, patch
+
 import pytest
-from unittest.mock import patch, MagicMock
 from main import service
 from requests.exceptions import ConnectionError
 
+
 @pytest.fixture
 def client():
-    service.config['TESTING'] = True
+    service.config["TESTING"] = True
     with service.test_client() as client:
         yield client
+
 
 @pytest.fixture
 def valid_data():
@@ -18,8 +21,9 @@ def valid_data():
         "weight": "71",
         "height": "198",
         "weight_loss_rate": 2,
-        "duration": 30
+        "duration": 30,
     }
+
 
 @pytest.fixture
 def invalid_data():
@@ -27,15 +31,17 @@ def invalid_data():
         "age": "abc",  # Invalid age
         "weight": "70",
         "height": "175",
-        "sex": "male"
+        "sex": "male",
     }
+
 
 def test_root_endpoint(client):
     """
     Test that the root endpoint returns a 200 status code
     """
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
+
 
 def test_submit_valid_data(client, valid_data):
     """
@@ -46,10 +52,11 @@ def test_submit_valid_data(client, valid_data):
         mock_response.status_code = 200
         mock_response.json.return_value = {"rmr": 1700}
         mock_post.return_value = mock_response
-        
+
         response = client.post("/submit/", data=valid_data)
         assert response.status_code == 200
         assert response.json == {"rmr": 1700}
+
 
 def test_submit_invalid_data(client, invalid_data):
     """
@@ -58,6 +65,7 @@ def test_submit_invalid_data(client, invalid_data):
     response = client.post("/submit/", data=invalid_data)
     assert response.status_code == 400
     assert "Invalid input data" in response.json["message"]
+
 
 def test_submit_api_failure(client, valid_data):
     """
@@ -69,12 +77,13 @@ def test_submit_api_failure(client, valid_data):
         assert response.status_code == 503
         assert "Failed to contact API" in response.json["error"]
 
+
 def test_health_check(client):
     """
     Test that the health check endpoint returns a 200 status code and healthy
     status
     """
-    response = client.get('/health')
+    response = client.get("/health")
     assert response.status_code == 200
     assert response.json["status"] == "healthy"
     assert "timestamp" in response.json
