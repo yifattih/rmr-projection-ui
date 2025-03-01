f0b819ea1b43ce23fdb0467687bd4286ca07a9c1
yifattih <yifattih@proton.me>
2025-03-01 06:50:44 +0000

    fix(service): add environmental variables to initialize before pytest to ignore instrumentation during tests

---
+++ service/tests/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+# Disable OpenTelemetry BEFORE FastAPI starts
+os.environ["OTEL_SDK_DISABLED"] = "true"
+os.environ["OTEL_TRACES_EXPORTER"] = "none"
+os.environ["OTEL_METRICS_EXPORTER"] = "none"
+os.environ["OTEL_LOGS_EXPORTER"] = "none"
