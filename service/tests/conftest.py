3131a57929bc6a19fc7af5d7566c353f1a8e696d
yifattih <yifattih@proton.me>
2025-03-02 05:44:44 +0000

    tests(service): add conftest.py to centralize fixtures and setup

---
+++ service/tests/conftest.py
@@ -0,0 +1,37 @@
+from main import service
+
+import pytest
+
+collect_ignore = ["otel/"]
+
+collect_ignore_glob = ["otel/*"]
+
+
+@pytest.fixture
+def client():
+    service.config["TESTING"] = True
+    with service.test_client() as client:
+        yield client
+
+
+@pytest.fixture
+def valid_data():
+    return {
+        "sex": "male",
+        "units": "imperial",
+        "age": "34",
+        "weight": "71",
+        "height": "198",
+        "weight_loss_rate": 2,
+        "duration": 30,
+    }
+
+
+@pytest.fixture
+def invalid_data():
+    return {
+        "age": "abc",  # Invalid age
+        "weight": "70",
+        "height": "175",
+        "sex": "male",
+    }
